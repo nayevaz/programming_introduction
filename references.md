ae9b2cfcc8c30739c36b0cbc95f83eee90d9080b
Antoine Lebel <ALebel@cegepgarneau.ca>
2023-03-03 14:47:47 -0500

    ref and live week3

--- references.md
+++ references.md
@@ -3,3 +3,6 @@ https://code.visualstudio.com/docs/python/python-tutorial
 
 # Git
 https://www.atlassian.com/git/tutorials/atlassian-git-cheatsheet
+
+# Built-in
+https://docs.python.org/3/library/functions.html
---
+++ week3/live.py
@@ -0,0 +1,18 @@
+# First question
+#first_number = input("Enter first number: ")
+#second_number = input("Enter second number: ")
+
+#answer = int(first_number) * int(second_number)
+
+#print(answer)
+
+#Concatenation
+#first_name = input("Enter name: ")
+#last_name = input("Enter name: ")
+
+#answer = first_name + " " + last_name
+
+#print(answer)
+
+# Ord Char
+print(ord("a"))
